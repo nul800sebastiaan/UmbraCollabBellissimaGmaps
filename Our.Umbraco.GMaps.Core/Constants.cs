b251f2e34b1f6f34b749f2712a03819ec364deea
Sebastiaan Janssen <sebastiaan@umbraco.com>
2024-04-18 10:50:58 +0200

    Add DataEditor to allow for custom config items on the datatype

---
+++ Our.Umbraco.GMaps.Core/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Our.Umbraco.GMaps.Core
+{
+    internal static class Constants
+    {
+        public const string PluginName = "Gmaps";
+        public const string MapPropertyAlias = "Our.Umbraco.GMaps";
+    }
+}
---
+++ Our.Umbraco.GMaps.Core/GmapsDataEditor.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.PropertyEditors;
+
+namespace Our.Umbraco.GMaps.Core;
+
+[DataEditor(Constants.MapPropertyAlias, ValueType = ValueTypes.Json, ValueEditorIsReusable = true)]
+public class GmapsDataEditor : DataEditor
+{
+    public GmapsDataEditor(IDataValueEditorFactory dataValueEditorFactory)
+        : base(dataValueEditorFactory)
+    {
+    }
+}
